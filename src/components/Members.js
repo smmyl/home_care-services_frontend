4cc9ba5c1c11efdbff507eacc72d8cf74768ac84
Sammy <sammy.liaop@gmail.com>
2023-03-31 19:18:53 -0400

    pages set up little bit more editting to go

--- src/components/Members.js
+++ src/components/Members.js
@@ -1,77 +1,24 @@
 import {useState} from 'react'
 import EditMember from './members/Edit'
+import Member from './Member'
 
 const Members = (props) => {
-    const[edit, setEdit] = useState(false)
-
-    const toggleEdit = () => {
-        setEdit(!edit)
-    }
 
     return (
         <div class='page'>
-            <div class='workers'>
-                <div class='workerstop'>
+            <div class='members'>
+                <div class='memberstop'>
                     <p>Non sint praesentium qui iusto rerum vel obcaecati ipsam sed inventore molestiae aut perspiciatis repellendus et dolorem nobis et voluptatem sunt. Ad voluptatibus ullam et asperiores repellat non debitis atque ea modi repudiandae in nostrum sequi eos quam velit.</p>
                 </div>
-                <div class='workersinfo'>
                 {props.members.map((member) => {
                     return(
-                    <div>
-                        {edit ?
-                            <>
-                            <h2>Edit</h2>
-                            <EditMember
-                                setMembers = {props.setMembers}
-                                getMembers = {props.getMembers}
-                                member = {member}
-                                toggleEdit = {toggleEdit}
-                                setEdit = {setEdit}
-                            /> 
-                            </>
-                            :
-                            <div class='workerinfo'>
-                                <div class='workerinfoinfo'>
-                                <h4>{member.name}</h4>
-                                <p>Age: {member.age}</p>
-                                <p>Family Members: {member.familyMember}</p>
-                                <p>Languages: {member.language}</p>
-                                <p>Address: {member.address}</p>
-                                <p>Services Needed:</p>
-                                {member.clean ?
-                                    <>
-                                        <p>Cleaning</p>
-                                    </>
-                                    :
-                                    <>
-                                    </>
-                                }
-                                {member.food ?
-                                    <>
-                                        <p>Food Delivery</p>
-                                    </>
-                                    :
-                                    <>
-                                    </>
-                                }
-                                {member.watch ?
-                                    <>
-                                        <p>Monitor</p>
-                                    </>
-                                    :
-                                    <>
-                                    </>
-                                }
-                                <button onClick = {() => {toggleEdit()}}>Edit</button>
-                                </div>
-                                <div class='workerinfoimage'>
-                                    <img src={member.image}></img>
-                                </div>
-                            </div>}
-                    </div>
+                        <Member
+                            member={member}
+                            getMembers={props.getMembers}
+                            setMembers={props.setMembers}
+                        />
                     )
                 })}
-                </div>
             </div>
         </div>
     )
